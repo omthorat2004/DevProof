707ae3aa5f6b265baa44e19590cedc44332a3afd
OM Thorat <omthorat1005@gmail.com>
2025-08-06 08:37:51 -0700

    changed cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const githubRoutes = require('./routes/github')
 const app = express()
 connectDB()
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json())
 app.use(morgan('dev'))
 
